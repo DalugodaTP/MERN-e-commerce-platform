10a0efcdc66f686098cf2244fb3aca6be822a7a8
Tharindu <Dalugodatp@gmail.com>
2024-08-30 20:09:46 +0530

    login an exisitng user and issue a token

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { createUser } from "../controllers/userController.js";
+import { createUser, loginUser } from "../controllers/userController.js";
 
 //----------Create User-----------
 router.route('/').post(createUser);
+//--login and logout of the user--
+router.post('/auth', loginUser);
 
 export default router;
